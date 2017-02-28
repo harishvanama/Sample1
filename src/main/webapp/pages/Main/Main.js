3ac04935cac0bcaa01241edbd16c67e441fc2513
apptest <appTest1@wavemaker.com>
2017-02-28 06:18:49 +0000

    classicmodels] Files modified - [Main.variables.json,Main.html,Main.js,project-services.xml New files added - [classicmodels] Files modified - [Main.variables.json,Main.html,Main.js,project-services.xml,pom.xml,deployment.properties,development.properties]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -15,3 +15,17 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
     };
 
 }]);
+
+Application.$controller("grid1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("grid2Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
